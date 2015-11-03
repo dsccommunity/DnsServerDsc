5e82d9c57b806614168d4a2ea16cb54820ce13c7
Danny den Braver <Danny den Braver>
2015-11-03 11:56:30 +0100

    updated testscript

--- Tests/xDnsRecord.Tests.ps1
+++ Tests/xDnsRecord.Tests.ps1
@@ -13,9 +13,9 @@ $RepoRoot = (Resolve-Path $PSScriptRoot\..).Path
 
 $ModuleName = "MSFT_xDnsRecord"
 Import-Module (Join-Path $RepoRoot "DSCResources\$ModuleName\$ModuleName.psm1")
-#Import-Module DnsServer
+Import-Module xDnsServer
 
-Describe "xDnsRecord" {
+Describe "xDnsRecord Type A-record" {
     InModuleScope $ModuleName {
         $testParams = @{
             Name = "test"
@@ -55,6 +55,53 @@ Describe "xDnsRecord" {
         Context "Validate set method" {
             It "Calls Add-DnsServerResourceRecordA in the set method" {
                 Mock Add-DnsServerResourceRecordA { return $null } -Verifiable
+                Set-TargetResource @testParams 
+                Assert-VerifiableMocks
+            }
+        }
+    }
+}
+
+Describe "xDnsRecord Type C-name" {
+    InModuleScope $ModuleName {
+        $testParams = @{
+            Name = "test"
+            Zone = "contoso.com"
+            Target = "test.contoso.com"
+            Type = "C-name"
+        }
+
+        Context "Validate test method" {
+            It "Fails when no DNS record exists" {
+                Mock Get-TargetResource { return @{} }
+                Test-TargetResource @testParams | Should Be $false
+            }
+            It "Passes when record exists and target matches" {
+                Mock Get-TargetResource { 
+                    return @{
+                        Name = $testParams.Name
+                        Zone = $testParams.Zone
+                        Target = $testParams.Target
+                        Type = $testParams.Type
+                    }
+                } 
+                Test-TargetResource @testParams | Should Be $true
+            }
+            It "Fails when the record exists and target does not match" {
+                Mock Get-TargetResource { 
+                    return @{
+                        Name = $testParams.Name
+                        Zone = $testParams.Zone
+                        Target = "test2.contoso.com"
+                        Type = $testParams.Type
+                    }
+                }
+                Test-TargetResource @testParams | Should Be $false
+            }
+        }
+        Context "Validate set method" {
+            It "Calls Add-DnsServerResourceRecordCName in the set method" {
+                Mock Add-DnsServerResourceRecordCName { return $null } -Verifiable
                 Set-TargetResource @testParams 
                 Assert-VerifiableMocks
             }
