31f9c6268c0050ee83043dc11551324ae1025bca
Jason Walker <walkerjason@live.com>
2016-07-27 17:01:26 -0500

    Adding xDnsServerDirectoryPartition

---
+++ Examples/Example_xDnsServerDirectoryPartition.ps1
@@ -0,0 +1,22 @@
+﻿configuration DnsServerDirectoryPartition
+{
+    Import-DscResource -ModuleName xDnsServer
+
+    node localhost
+    {
+        xDnsServerDirectoryPartition AddContoso
+        {
+            Name = 'contoso.com'
+            Ensure = 'Present'
+        }
+        xDnsServerDirectoryPartition AddContoso
+        {
+            Name = 'child.contoso.com'
+            Ensure = 'Absent'
+        }
+    }
+}
+
+DnsServerDirectoryPartition -OutputPath C:\dscDns
+
+Start-DscConfiguration -Path C:\dscDNS -Wait -Force -Verbose
