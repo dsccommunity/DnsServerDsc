8fb65fd6f21a5d9a3fdb016f99fb42a801bcfe95
James Sudbury <james.sudbury@btinternet.com>
2021-02-17 03:03:19 +0000

    Initial addition of class-based record resources

---
+++ source/Classes/DSC_xDnsRecordBase.ps1
@@ -0,0 +1,71 @@
+$script:localizedDataxDnsRecordBase = Get-LocalizedData -DefaultUICulture en-US -FileName 'DSC_xDnsRecordBase.strings.psd1'
+
+[DscResource()]
+class DSC_xDnsRecordBase
+{
+    [DscProperty(Key)]
+    [string] $ZoneName
+
+    [DscProperty()]
+    [TimeSpan] $TimeToLive
+
+    [DscProperty()]
+    [string] $DnsServer = 'localhost'
+
+    [DscProperty()]
+    [bool] $AgeRecord = $false
+
+    [DscProperty()]
+    [Ensure] $Ensure
+
+    #region Generic DSC methods -- DO NOT OVERRIDE
+
+    [DSC_xDnsRecordBase] Get()
+    {
+        $record = $this.GetResourceRecord()
+        $dscResourceObject = $null
+        if ($null -eq $record)
+        {
+            <#
+                Create an object of the correct type (i.e.: the subclassed resource type)
+                and set its values to those specified in the object, but set Ensure to Absent
+            #>
+            $dscResourceObject = [activator]::CreateInstance($this.GetType())
+            foreach ($propertyName in $this.GetType().GetProperties().Name)
+            {
+                $dscResourceObject.$propertyName = $this.$propertyName
+            }
+            $dscResourceObject.Ensure = "Absent"
+        } else {
+            # Build an object reflecting the current state based on the record found
+            $dscResourceObject = $this.NewDscResourceObjectFromRecord($record)
+        }
+        return $dscResourceObject
+    }
+
+    [void] Set()
+    {
+        throw "Set() not implemented"
+    }
+
+    [bool] Test()
+    {
+        throw "Test() not implemented"
+    }
+
+    #endregion
+
+    #region Methods to override
+
+    hidden [ciminstance] GetResourceRecord()
+    {
+        throw "GetResourceRecord() not implemented"
+    }
+
+    hidden [DSC_xDnsRecordBase] NewDscResourceObjectFromRecord([ciminstance] $record)
+    {
+        throw "NewResourceObjectFromRecord() not implemented"
+    }
+
+    #endregion
+}
