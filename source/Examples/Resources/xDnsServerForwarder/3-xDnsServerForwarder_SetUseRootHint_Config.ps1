1f913447b9ebe47f670872cb9f346ab1a1aee3a0
Johan Ljunggren <johan.ljunggren@shamn.se>
2021-02-24 16:27:09 +0100

    Add example

---
+++ source/Examples/Resources/xDnsServerForwarder/3-xDnsServerForwarder_SetUseRootHint_Config.ps1
@@ -0,0 +1,54 @@
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID 253542b-7448-4379-8272-fed7babe5e1d
+
+.AUTHOR DSC Community
+
+.COMPANYNAME DSC Community
+
+.COPYRIGHT DSC Community contributors. All rights reserved.
+
+.TAGS DSCConfiguration
+
+.LICENSEURI https://github.com/dsccommunity/xDnsServer/blob/main/LICENSE
+
+.PROJECTURI https://github.com/dsccommunity/xDnsServer
+
+.ICONURI https://dsccommunity.org/images/DSC_Logo_300p.png
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+Updated author, copyright notice, and URLs.
+
+.PRIVATEDATA 2016-Datacenter,2016-Datacenter-Server-Core
+
+#>
+
+#Requires -Module xDnsServer
+
+
+<#
+    .DESCRIPTION
+        This configuration will remove all the DNS forwarders
+#>
+
+Configuration xDnsServerForwarder_SetUseRootHint_Config
+{
+    Import-DscResource -ModuleName 'xDnsServer'
+
+    Node localhost
+    {
+        xDnsServerForwarder 'SetUseRootHints'
+        {
+            IsSingleInstance = 'Yes'
+            UseRootHint      = $true
+        }
+    }
+}
