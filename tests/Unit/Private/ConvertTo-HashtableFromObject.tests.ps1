cb22c94263318d2d945be76531e985ad7a450fbd
James Sudbury <james.sudbury@btinternet.com>
2021-03-14 02:52:44 +0000

    renaming

--- tests/Unit/Private/ConvertTo-HashtableFromObject.tests.ps1
+++ tests/Unit/Private/ConvertTo-HashtableFromObject.tests.ps1
@@ -1,48 +1,46 @@
 $ProjectPath = "$PSScriptRoot\..\..\.." | Convert-Path
 $ProjectName = (Get-ChildItem $ProjectPath\*\*.psd1 | Where-Object -FilterScript {
         ($_.Directory.Name -match 'source|src' -or $_.Directory.Name -eq $_.BaseName) -and
-        $(try { Test-ModuleManifest $_.FullName -ErrorAction Stop }catch{$false}) }
-    ).BaseName
+        $(try
+            {
+                Test-ModuleManifest $_.FullName -ErrorAction Stop
+            }
+            catch
+            {
+                $false
+            }) }
+).BaseName
 
 Import-Module $ProjectName
 
 InModuleScope $ProjectName {
-
     Describe 'Helper function ConvertTo-HashtableFromObject' -Tag 'Private' {
         BeforeAll {
             $script:mockItemName = 'contoso.com'
-            $script:mockItem     = [pscustomobject]@{
-                Name                       = $script:mockItemName
-                ZoneName          = 'contoso.com'
-                SymbolicName      = 'xmpp'
-            }
         }
 
-        BeforeEach{
-            $script:instanceDesiredState = [DnsRecordSrv]::new()
-            $script:instanceDesiredState.ZoneName = $script:mockItemName
-            $script:instanceDesiredState.Ensure = [Ensure]::Present
+        BeforeEach {
+            $script:dscResourceObject = [DnsRecordSrv]::new()
+            $script:dscResourceObject.ZoneName = $script:mockItemName
+            $script:dscResourceObject.Ensure = [Ensure]::Present
         }
 
         Context 'When instance of class is convert to hashtable' {
-            BeforeEach {
-
-            }
             It 'Should not Throw' {
-                {$script:convertHashtable = $script:instanceDesiredState | ConvertTo-HashtableFromObject} | Should -Not -Throw
+                { $script:conversionResult = $script:dscResourceObject | ConvertTo-HashtableFromObject } | Should -Not -Throw
             }
 
             It 'Should be a Hashtable' {
-                $script:convertHashtable | Should -BeOfType [hashtable]
+                $script:conversionResult | Should -BeOfType [hashtable]
             }
 
             It 'Should have the same count of properties' {
-                $script:convertHashtable.keys.count | Should -Be $script:instanceDesiredState.psobject.Properties.Name.count
+                $script:conversionResult.keys.count | Should -Be $script:dscResourceObject.psobject.Properties.Name.count
             }
             It 'Should be the same value of key' {
-                $script:instanceDesiredState.psobject.Properties.Name | ForEach-Object {
-                    $script:convertHashtable.ContainsKey($_) | Should -BeTrue
-                    $script:convertHashtable.$_ | Should -Be $instanceDesiredState.$_
+                $script:dscResourceObject.psobject.Properties.Name | ForEach-Object {
+                    $script:conversionResult.ContainsKey($_) | Should -BeTrue
+                    $script:conversionResult.$_ | Should -Be $dscResourceObject.$_
                 }
             }
         }
