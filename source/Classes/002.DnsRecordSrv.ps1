1b3b12db7282aa0f620abc0b7dc8e16a4a083198
James Sudbury <james.sudbury@btinternet.com>
2021-03-15 01:46:07 +0000

    Removed parameter help specified by parent classes.

--- source/Classes/002.DnsRecordSrv.ps1
+++ source/Classes/002.DnsRecordSrv.ps1
@@ -22,18 +22,6 @@
 
     .PARAMETER Weight
         Specifies the weight of the SRV record. (Mandatory Parameter)
-
-    .PARAMETER ZoneName
-        Specifies the name of a DNS zone. (Key Parameter)
-
-    .PARAMETER TimeToLive
-        Specifies the TimeToLive value of the SRV record. Value must be in valid TimeSpan string format (i.e.: Days.Hours:Minutes:Seconds.Miliseconds or 30.23:59:59.999).
-
-    .PARAMETER DnsServer
-        Name of the DnsServer on which to create.
-
-    .PARAMETER Ensure
-        Whether the host record should be present or removed.
 #>
 
 $script:localizedDataDnsRecordSrv = Get-LocalizedData -DefaultUICulture 'en-US' -FileName 'DnsRecordSrv.strings.psd1'
--- source/Classes/003.DnsRecordSrvScoped.ps1
+++ source/Classes/003.DnsRecordSrvScoped.ps1
@@ -5,38 +5,8 @@
     .DESCRIPTION
         The DnsRecordSrvScoped DSC resource manages SRV DNS records against a specific zone and zone scope on a Domain Name System (DNS) server.
 
-    .PARAMETER SymbolicName
-        Service name for the SRV record. eg: xmpp, ldap, etc. (Key Parameter)
-
-    .PARAMETER Protocol
-        Service transmission protocol ('TCP' or 'UDP') (Key Parameter)
-
-    .PARAMETER Port
-        The TCP or UDP port on which the service is found (Key Parameter)
-
-    .PARAMETER Target
-        Specifies the Target Hostname or IP Address. (Key Parameter)
-
-    .PARAMETER Priority
-        Specifies the Priority value of the SRV record. (Mandatory Parameter)
-
-    .PARAMETER Weight
-        Specifies the weight of the SRV record. (Mandatory Parameter)
-
-    .PARAMETER ZoneName
-        Specifies the name of a DNS zone. (Key Parameter)
-
     .PARAMETER ZoneScope
         Specifies the name of a zone scope. (Key Parameter)
-
-    .PARAMETER TimeToLive
-        Specifies the TimeToLive value of the SRV record. Value must be in valid TimeSpan string format (i.e.: Days.Hours:Minutes:Seconds.Miliseconds or 30.23:59:59.999).
-
-    .PARAMETER DnsServer
-        Name of the DnsServer to create the record on.
-
-    .PARAMETER Ensure
-        Whether the host record should be present or removed.
 #>
 
 $script:localizedDataDnsRecordSrvScoped = Get-LocalizedData -DefaultUICulture 'en-US' -FileName 'DnsRecordSrvScoped.strings.psd1'
