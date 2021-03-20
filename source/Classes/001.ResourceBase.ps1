e0cc4b5ed173c0169a35a7039b309c7c47a7f8e4
Johan Ljunggren <johan.ljunggren@shamn.se>
2021-03-20 14:48:39 +0100

    DnsServerScavenging: Add new resource

--- source/Classes/001.ResourceBase.ps1
+++ source/Classes/001.ResourceBase.ps1
@@ -6,7 +6,8 @@
        A class with DSC properties that are equal for all class-based resources.
 
     .PARAMETER DnsServer
-        Name of the DnsServer on which to create the record.
+        The host name of the Domain Name System (DNS) server, or use 'localhost'
+        for the current node. Defaults to `'localhost'`.
 #>
 
 class ResourceBase
@@ -15,8 +16,94 @@ class ResourceBase
     [System.String]
     $DnsServer = 'localhost'
 
+    # Hidden property for holding localization strings
+    hidden [System.Collections.Hashtable] $localizedData = @{}
+
     # Default constructor
-    DnsRecordBase()
+    ResourceBase()
     {
+        Assert-Module -ModuleName 'DnsServer'
+
+        $localizedDataFileName = ('{0}.strings.psd1' -f $this.GetType().Name)
+
+        $this.localizedData = Get-LocalizedData -DefaultUICulture 'en-US' -FileName $localizedDataFileName
+    }
+
+    [ResourceBase] Get([Microsoft.Management.Infrastructure.CimInstance] $CurrentState)
+    {
+        $dscResourceObject = [System.Activator]::CreateInstance($this.GetType())
+
+        foreach ($propertyName in $this.PSObject.Properties.Name)
+        {
+            $dscResourceObject.$propertyName = $CurrentState.$propertyName
+        }
+
+        return $dscResourceObject
+    }
+
+    [void] Set()
+    {
+    }
+
+    [System.Boolean] Test()
+    {
+        $isInDesiredState = $true
+
+        <#
+            Returns all enforced properties not in desires state, or $null if
+            all enforced properties are in desired state.
+        #>
+        $propertiesNotInDesiredState = $this.Compare()
+
+        if ($propertiesNotInDesiredState)
+        {
+            $isInDesiredState = $false
+        }
+
+        return $isInDesiredState
+    }
+
+    # Returns a hashtable containing all properties that should be enforced.
+    hidden [System.Collections.Hashtable] Compare()
+    {
+        $currentState = $this.Get() | ConvertTo-HashTableFromObject
+        $desiredState = $this | ConvertTo-HashTableFromObject
+
+        # Remove properties that have $null as the value.
+        @($desiredState.Keys) | ForEach-Object -Process {
+            $isReadProperty = $this.GetType().GetMember($_).CustomAttributes.Where( { $_.NamedArguments.MemberName -eq 'NotConfigurable' }).NamedArguments.TypedValue.Value -eq $true
+
+            # Also remove read properties so that there is no chance to campare those.
+            if ($isReadProperty -or $null -eq $desiredState[$_])
+            {
+                $desiredState.Remove($_)
+            }
+        }
+
+        $CompareDscParameterState = @{
+            CurrentValues     = $currentState
+            DesiredValues     = $desiredState
+            Properties        = $desiredState.Keys
+            ExcludeProperties = @('DnsServer')
+            IncludeValue      = $true
+        }
+
+        <#
+            Returns all enforced properties not in desires state, or $null if
+            all enforced properties are in desired state.
+        #>
+        return (Compare-DscParameterState @CompareDscParameterState)
+    }
+
+    # Returns a hashtable containing all properties that should be enforced.
+    hidden [System.Collections.Hashtable] GetDesiredStateForSplatting([System.Collections.Hashtable] $Properties)
+    {
+        $desiredState = @{}
+
+        $Properties | ForEach-Object -Process {
+            $desiredState[$_.Property] = $_.ExpectedValue
+        }
+
+        return $desiredState
     }
 }
---
+++ source/Classes/003.DnsServerScavenging.ps1
@@ -0,0 +1,141 @@
+<#
+    .SYNOPSIS
+        The DnsServerScavenging DSC resource manages scavenging on a Microsoft
+        Domain Name System (DNS) server.
+
+    .DESCRIPTION
+        The DnsServerScavenging DSC resource manages scavenging on a Microsoft
+        Domain Name System (DNS) server.
+
+    .PARAMETER DnsServer
+        The host name of the Domain Name System (DNS) server, or use 'localhost'
+        for the current node.
+
+    .PARAMETER ScavengingState
+        Specifies whether to Enable automatic scavenging of stale records.
+        `ScavengingState` determines whether the DNS scavenging feature is enabled
+        by default on newly created zones.
+
+    .PARAMETER ScavengingInterval
+        Specifies a length of time as a value that can be converted to a `[TimeSpan]`
+        object. `ScavengingInterval` determines whether the scavenging feature for
+        the DNS server is enabled and sets the number of hours between scavenging
+        cycles. The value `0` disables scavenging for the DNS server. A setting
+        greater than `0` enables scavenging for the server and sets the number of
+        days, hours, minutes, and seconds (formatted as dd.hh:mm:ss) between
+        scavenging cycles. The minimum value is 0. The maximum value is 365.00:00:00
+        (1 year).
+
+    .PARAMETER RefreshInterval
+        Specifies the refresh interval as a value that can be converted to a `[TimeSpan]`
+        object (formatted as dd.hh:mm:ss). During this interval, a DNS server can
+        refresh a resource record that has a non-zero time stamp. Zones on the server
+        inherit this value automatically. If a DNS server does not refresh a resource
+        record that has a non-zero time stamp, the DNS server can remove that record
+        during the next scavenging. Do not select a value smaller than the longest
+        refresh period of a resource record registered in the zone. The minimum value
+        is `0`. The maximum value is 365.00:00:00 (1 year).
+
+    .PARAMETER NoRefreshInterval
+        Specifies a length of time as a value that can be converted to a `[TimeSpan]`
+        object (formatted as dd.hh:mm:ss). `NoRefreshInterval` sets a period of time
+        in which no refreshes are accepted for dynamically updated records. Zones on
+        the server inherit this value automatically. This value is the interval between
+        the last update of a timestamp for a record and the earliest time when the
+        timestamp can be refreshed. The minimum value is 0. The maximum value is
+        365.00:00:00 (1 year).
+
+    .PARAMETER LastScavengeTime
+        The time when the last scavenging cycle was executed.
+#>
+
+[DscResource()]
+class DnsServerScavenging : ResourceBase
+{
+    # The property DnsServer is overridden from the base class.
+    [DscProperty(Key)]
+    [System.String]
+    $DnsServer
+
+    [DscProperty()]
+    [Nullable[System.Boolean]]
+    $ScavengingState
+
+    [DscProperty()]
+    [System.String]
+    $ScavengingInterval
+
+    [DscProperty()]
+    [System.String]
+    $RefreshInterval
+
+    [DscProperty()]
+    [System.String]
+    $NoRefreshInterval
+
+    [DscProperty(NotConfigurable)]
+    [Nullable[System.DateTime]]
+    $LastScavengeTime
+
+    # Default constructor.
+    DnsServerScavenging() : base ()
+    {
+    }
+
+    [DnsServerScavenging] Get()
+    {
+        Write-Verbose -Message ($this.localizedData.GetCurrentState -f $this.DnsServer)
+
+        $getDnsServerScavengingParameters = @{}
+
+        if ($this.DnsServer -ne 'localhost')
+        {
+            $getDnsServerScavengingParameters['ComputerName'] = $this.DnsServer
+        }
+
+        $getDnsServerScavengingResult = Get-DnsServerScavenging @getDnsServerScavengingParameters
+
+        # Call the base method to return the properties.
+        return [DnsServerScavenging] ([ResourceBase] $this).Get($getDnsServerScavengingResult)
+    }
+
+    [void] Set()
+    {
+        Write-Verbose -Message ($this.localizedData.SetDesiredState -f $this.DnsServer)
+
+        # Get enforced properties that are not in desired state.
+        $propertiesNotInDesiredState = $this.Compare()
+
+        $setDnsServerScavengingParameters = $this.GetDesiredStateForSplatting($propertiesNotInDesiredState)
+
+        $setDnsServerScavengingParameters.Keys | ForEach-Object -Process {
+            Write-Verbose -Message ($this.localizedData.SetProperty -f $_, $setDnsServerScavengingParameters.$_)
+        }
+
+        if ($this.DnsServer -ne 'localhost')
+        {
+            $setDnsServerScavengingParameters['ComputerName'] = $this.DnsServer
+        }
+
+        Set-DnsServerScavenging @setDnsServerScavengingParameters
+    }
+
+    [System.Boolean] Test()
+    {
+        Write-Verbose -Message ($this.localizedData.TestDesiredState -f $this.DnsServer)
+
+        # Call the base method to test all of the properties that should be enforced.
+        $isInDesiredState = ([ResourceBase] $this).Test()
+
+        if ($isInDesiredState)
+        {
+            Write-Verbose -Message ($this.localizedData.InDesiredState -f $this.DnsServer)
+        }
+        else
+        {
+            Write-Verbose -Message ($this.localizedData.NotInDesiredState -f $this.DnsServer)
+        }
+
+        return $isInDesiredState
+    }
+}
