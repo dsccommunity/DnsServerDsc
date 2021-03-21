5778b98f3fa61eaf1a8875474a3d3d3911f99d68
Johan Ljunggren <johan.ljunggren@shamn.se>
2021-03-21 10:09:58 +0100

    Remove DnsServerDsc.Common from prefix.ps1

--- source/prefix.ps1
+++ source/prefix.ps1
@@ -1,7 +1,3 @@
-# Import nested, 'DnsServerDsc.Common' module
-$script:dnsServerDscCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\DnsServerDsc.Common'
-Import-Module -Name $script:dnsServerDscCommonModulePath
-
 # Import nested, 'DscResource.Common' module
 $script:dscResourceCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\DscResource.Common'
 Import-Module -Name $script:dscResourceCommonModulePath
