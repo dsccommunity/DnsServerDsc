dac9db453aa8cf1c8d42bb0b6145b7b26f48f4a0
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-07-23 18:23:06 +0100

    Assert-TimeSpan fix exception message tests

--- source/en-US/DnsServerDsc.strings.psd1
+++ source/en-US/DnsServerDsc.strings.psd1
@@ -6,7 +6,7 @@
 #>
 
 ConvertFrom-StringData @'
-    PropertyHasWrongFormat = The property '{0}' has the value '{1}' that cannot be converted to [System.TimeSpan]. (DS0001)
+    PropertyHasWrongFormat = The property '{0}' has the value '{1}' that cannot be converted to 'System.TimeSpan'. (DS0001)
     TimeSpanExceedMaximumValue = The property '{0}' has the value '{1}' that exceeds the maximum value of '{2}'. (DS0002)
     TimeSpanBelowMinimumValue = The property '{0}' has the value '{1}' that is below the minimum value of '{2}'. (DS0003)
 '@
--- tests/Unit/Private/Assert-TimeSpan.Tests.ps1
+++ tests/Unit/Private/Assert-TimeSpan.Tests.ps1
@@ -1,55 +1,102 @@
-$ProjectPath = "$PSScriptRoot\..\..\.." | Convert-Path
-$ProjectName = (Get-ChildItem $ProjectPath\*\*.psd1 | Where-Object -FilterScript {
-        ($_.Directory.Name -match 'source|src' -or $_.Directory.Name -eq $_.BaseName) -and
-        $(try
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param ()
+
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
             {
-                Test-ModuleManifest $_.FullName -ErrorAction Stop
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../../build.ps1" -Tasks 'noop' 2>&1 4>&1 5>&1 6>&1 > $null
             }
-            catch
-            {
-                $false
-            }) }
-).BaseName
 
-Import-Module $ProjectName -Force
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+}
 
-InModuleScope $ProjectName {
-    Describe 'Assert-TimeSpan' -Tag 'Private' {
-        Context 'When asserting a valid time' {
-            Context 'When passing value with named parameter' {
-                It 'Should not throw an exception' {
+BeforeAll {
+    $script:dscModuleName = 'DnsServerDsc'
+
+    Import-Module -Name $script:dscModuleName
+
+    $PSDefaultParameterValues['InModuleScope:ModuleName'] = $script:dscModuleName
+    $PSDefaultParameterValues['Mock:ModuleName'] = $script:dscModuleName
+    $PSDefaultParameterValues['Should:ModuleName'] = $script:dscModuleName
+}
+
+AfterAll {
+    $PSDefaultParameterValues.Remove('InModuleScope:ModuleName')
+    $PSDefaultParameterValues.Remove('Mock:ModuleName')
+    $PSDefaultParameterValues.Remove('Should:ModuleName')
+
+    # Unload the module being tested so that it doesn't impact any other tests.
+    Get-Module -Name $script:dscModuleName -All | Remove-Module -Force
+}
+
+Describe 'Assert-TimeSpan' -Tag 'Private' {
+    Context 'When asserting a valid time' {
+        Context 'When passing value with named parameter' {
+            It 'Should not throw an exception' {
+                InModuleScope -ScriptBlock {
+                    Set-StrictMode -Version 1.0
+
                     { Assert-TimeSpan -PropertyName 'MyProperty' -Value '1.00:00:00' } | Should -Not -Throw
                 }
             }
+        }
 
-            Context 'When passing value in pipeline' {
-                It 'Should not throw an exception' {
+        Context 'When passing value in pipeline' {
+            It 'Should not throw an exception' {
+                InModuleScope -ScriptBlock {
+                    Set-StrictMode -Version 1.0
+
                     { '1.00:00:00' | Assert-TimeSpan -PropertyName 'MyProperty' } | Should -Not -Throw
                 }
             }
         }
+    }
 
-        Context 'When asserting a invalid string' {
-            It 'Should throw the correct error message' {
-                $mockExpectedErrorMessage = $script:localizedData.PropertyHasWrongFormat -f 'MyProperty', 'a.00:00:00'
+    Context 'When asserting a invalid string' {
+        It 'Should throw the correct error message' {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
 
-                { 'a.00:00:00' | Assert-TimeSpan -PropertyName 'MyProperty' } | Should -Throw $mockExpectedErrorMessage
+                $mockErrorMessage = $script:localizedData.PropertyHasWrongFormat -f 'MyProperty', 'a.00:00:00'
+
+                { 'a.00:00:00' | Assert-TimeSpan -PropertyName 'MyProperty' } | Should -Throw -ExpectedMessage ('*' + $mockErrorMessage)
             }
         }
+    }
 
-        Context 'When time is above maximum allowed value' {
-            It 'Should throw the correct error message' {
-                $mockExpectedErrorMessage = $script:localizedData.TimeSpanExceedMaximumValue -f 'MyProperty', '1.00:00:00', '00:30:00'
+    Context 'When time is above maximum allowed value' {
+        It 'Should throw the correct error message' {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
 
-                { '1.00:00:00' | Assert-TimeSpan -PropertyName 'MyProperty' -Maximum '0.00:30:00' } | Should -Throw $mockExpectedErrorMessage
+                $mockErrorMessage = $script:localizedData.TimeSpanExceedMaximumValue -f 'MyProperty', '1.00:00:00', '00:30:00'
+
+                { '1.00:00:00' | Assert-TimeSpan -PropertyName 'MyProperty' -Maximum '0.00:30:00' } | Should -Throw -ExpectedMessage ('*' + $mockErrorMessage)
             }
         }
+    }
 
-        Context 'When time is below minimum allowed value' {
-            It 'Should throw the correct error message' {
-                $mockExpectedErrorMessage = $script:localizedData.TimeSpanBelowMinimumValue -f 'MyProperty', '1.00:00:00', '2.00:00:00'
+    Context 'When time is below minimum allowed value' {
+        It 'Should throw the correct error message' {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
 
-                { '1.00:00:00' | Assert-TimeSpan -PropertyName 'MyProperty' -Minimum '2.00:00:00' } | Should -Throw $mockExpectedErrorMessage
+                $mockErrorMessage = $script:localizedData.TimeSpanBelowMinimumValue -f 'MyProperty', '1.00:00:00', '2.00:00:00'
+
+                { '1.00:00:00' | Assert-TimeSpan -PropertyName 'MyProperty' -Minimum '2.00:00:00' } | Should -Throw -ExpectedMessage ('*' + $mockErrorMessage)
             }
         }
     }
