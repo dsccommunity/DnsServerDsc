2e5316a9847e513024544b9e2caa4f2227e5e9c1
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-08-18 15:50:15 +0100

    Update description

--- source/Classes/001.DnsServerReason.ps1
+++ source/Classes/001.DnsServerReason.ps1
@@ -5,7 +5,7 @@
     .DESCRIPTION
         A DSC resource can have a read-only property `Reasons` that the compliance
         part (audit via Azure Policy) of Azure AutoManage Machine Configuration
-        uses. The property Reasons holds an array of SqlReason. Each SqlReason
+        uses. The property Reasons holds an array of DnsServerReason. Each DnsServerReason
         explains why a property of a DSC resource is not in desired state.
 #>
 
