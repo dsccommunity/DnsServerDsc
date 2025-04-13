ca9a3bc90067a334018134fa485bfcc66f27fba3
EJansenMSFT <EJansenMSFT@users.noreply.github.com>
2025-04-12 23:28:51 -0400

    Updated code based on most of the recommendations from Chris - but still need to test.

--- source/DSCResources/DSC_DNSServerStubZone/DSC_DnsServerStubZone.psm1
+++ source/DSCResources/DSC_DNSServerStubZone/DSC_DnsServerStubZone.psm1
@@ -16,15 +16,15 @@ function Get-TargetResource
         [System.String]
         $Name,
 
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [ipaddress[]]
+        $MasterServers,
+
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [System.String]
         $ZoneFile = "$Name.dns",
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string[]]
-        $MasterServers,
 
         [Parameter()]
         [ValidateSet('Present','Absent')]
@@ -37,13 +37,12 @@ function Get-TargetResource
     Write-Verbose ($script:localizedData.CheckingZoneMessage -f $Name, $Ensure)
 
     $dnsServerZone = Get-DnsServerZone -Name $Name -ErrorAction SilentlyContinue
-  
 
     $targetResource = @{
         Name = $Name
+        MasterServers = $dnsServerZone.MasterServers | ForEach-Object { $_.IPAddressToString }
         ZoneFile = $dnsServerZone.ZoneFile
-        MasterServers = $dnsServerZone.MasterServers | ForEach-Object { $_.IPAddressToString } | Sort-Object
-        Ensure = if ($null -eq $dnsServerZone) { 'Absent' } else { 'Present' }
+        Ensure = if (-not $dnsServerZone) { 'Absent' } else { 'Present' }
     }
 
     return $targetResource
@@ -60,15 +59,15 @@ function Test-TargetResource
         [System.String]
         $Name,
 
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [ipaddress[]]
+        $MasterServers,
+
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [System.String]
         $ZoneFile = "$Name.dns",
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string[]]
-        $MasterServers,
 
         [Parameter()]
         [ValidateSet('Present','Absent')]
@@ -77,54 +76,82 @@ function Test-TargetResource
     )
 
     $targetResource = Get-TargetResource @PSBoundParameters
-
     $targetResourceInCompliance = $true
+    $dnsServerZone = Get-DnsServerZone -Name $Name -ErrorAction SilentlyContinue
 
-    #If we specify that we want to ensure the zone should be PRESENT, check compliance.
-    if ($Ensure -eq 'Present')
+    #If we specify that we want to ensure the zone should be ABSENT, check compliance.
+    if ($Ensure -eq 'Absent')
     {
-        #If the results of our Get-TargetResource shows that the zone is PRESENT, move on to the next validation check.
+        #If the results of our Get-TargetResource shows that the zone is PRESENT, set compliance, because we want it to be absent.
         if ($targetResource.Ensure -eq 'Present')
         {
-            #If the zonefile name doesn't equal what we defined, set non-compliance.
-            if ($targetResource.ZoneFile -ne $ZoneFile)
-            {
-                #ZoneFile name differs from the desired configuration                 0}'         '{1}'                  '{2}'     <--- Definitions in the DSC_DnsServerStubZone.strings.psd1
-                Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'ZoneFile', $targetResource.ZoneFile, $ZoneFile)
-
-                $targetResourceInCompliance = $false
-            }
-            #If the Master Servers differ from the desired configuration, set non-compliance.
-            if (($targetResource.MasterServers  -join ',' | Sort-Object) -ne ($MasterServers -join ',' | Sort-Object))
-            {
-                #Zone  Master Servers differ from the desired configuration             '{0}'            '{1}'                                '{2}'     <--- Definitions in the DSC_DnsServerStubZone.strings.psd1
-                Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'MasterServers', ($MasterServers -join ','), ($targetResource.MasterServers -join ','))
-                $targetResourceInCompliance = $false
-            }
-        }
-        #Otherwise, if the results of our Get-TargetResource shows that the zone is ABSENT, set non-compliance, because we want it to exist.
-        else
-        {
-            # Dns zone is absent and should be present.                        '{0}'      '{1}'     '{2}'     <--- Definitions in the DSC_DnsServerStubZone.strings.psd1
-            Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'Ensure', 'Present', 'Absent')
-
-            $targetResourceInCompliance = $false
-        }
-    }
-    #Otherwise, if $Ensure is not PRESENT we specify that means we want to ensure the zone is ABSENT, so check compliance.
-    else
-    {
-        #If the results of our Get-TargetResource shows that the zone is present, set non-compliance.
-        if ($targetResource.Ensure -eq 'Present')
-        {
-            # Dns zone is present and needs removing.                          '{0}'      '{1}'     '{2}'     <--- Definitions in the DSC_DnsServerStubZone.strings.psd1
+            # Dns zone is present and should be absent.                        '{0}'      '{1}'     '{2}'     <--- Definitions in the DSC_DnsServerStubZone.strings.psd1
             Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'Ensure', 'Absent', 'Present')
 
             $targetResourceInCompliance = $false
+            return $targetResourceInCompliance
+
         }
     }
+    #If we specify that we want to ensure the zone should be PRESENT, check compliance.
+    if ($Ensure -eq 'Present')
+    {
 
-    return $targetResourceInCompliance
+        #If the results of our Get-TargetResource shows that the zone is PRESENT, set compliance, because we want it to be absent.
+        if ($targetResource.Ensure -eq 'Absent')
+        {
+            # Dns zone is absent and should be present.
+            Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'Ensure', 'Present', 'Absent')
+
+            $targetResourceInCompliance = $false
+            return $targetResourceInCompliance
+        }
+
+        #If the results of our Get-TargetResource shows that the zone is PRESENT, move on to the next validation check.
+        if ($targetResource.Ensure -eq 'Present')
+        {
+
+            #If the Zone is AD integrated, set non-compliance.
+            if ($dnsServerZone.IsDSIntegrated -eq $true)
+            {
+                #Zone Storage differs from the desired configuration
+                Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'ZoneStorageLocation', 'Active Directory', 'File')
+
+                $targetResourceInCompliance = $false
+                return $targetResourceInCompliance
+            }
+            #If the ZoneType isn't of type Stub, set non-compliance.
+            if ($dnsServerZone.ZoneType -ne 'Stub')
+            {
+                #Zone Type differs from the desired configuration
+                Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'ZoneType', 'Stub', $targetResource.AllZoneData.ZoneType)
+
+                $targetResourceInCompliance = $false
+                return $targetResourceInCompliance
+            }
+            #If the Zone File name differ from the desired configuration, set non-compliance.
+            if ($targetResource.ZoneFile -ne $ZoneFile)
+            {
+                #ZoneFile name differs from the desired configuration
+                Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'ZoneFile', $targetResource.ZoneFile, $ZoneFile)
+
+                $targetResourceInCompliance = $false
+                return $targetResourceInCompliance
+            }
+            #If the Master Servers differ from the desired configuration, set non-compliance.
+            $Comparison = Compare-Object -ReferenceObject $MasterServers -DifferenceObject $targetResource.MasterServers
+            if ($Comparison)
+            {
+                #Zone  Master Servers differ from the desired configuration
+                Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'MasterServers', ($MasterServers -join ','), ($targetResource.MasterServers -join ','))
+                $targetResourceInCompliance = $false
+            }
+
+            return $targetResourceInCompliance
+
+        }
+
+    }
 
 } #end function Test-TargetResource
 
@@ -137,15 +164,15 @@ function Set-TargetResource
         [System.String]
         $Name,
 
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [ipaddress[]]
+        $MasterServers,
+
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [System.String]
         $ZoneFile = "$Name.dns",
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string[]]
-        $MasterServers,
 
         [Parameter()]
         [ValidateSet('Present','Absent')]
@@ -155,20 +182,44 @@ function Set-TargetResource
 
     Assert-Module -ModuleName 'DNSServer'
 
+    if ($Ensure -eq 'Absent')
+    {
+        Write-Verbose ($script:localizedData.CheckingZoneMessage -f $Name, $Ensure)
+
+        IF($dnsServerZone.type -eq 'Stub' -and $dnsServerZone.IsDSIntegrated -eq $false)
+        {
+
+        # Remove the DNS Server zone.
+        Write-Verbose ($script:localizedData.RemovingZoneMessage -f $Name)
+        Get-DnsServerZone -Name $Name | Remove-DnsServerZone -Force
+
+        }
+        else
+        {
+            IF($dnsServerZone.type -ne 'Stub'){
+
+                # Zone is not a stub zone.
+                Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'ZoneType', 'Stub', $dnsServerZone.ZoneType)
+            }
+            If($dnsServerZone.IsDSIntegrated -ne $false){
+
+                # Zone is AD integrated.
+                Write-Verbose ($script:localizedData.NotDesiredPropertyMessage -f 'ZoneStorageLocation', 'Active Directory', 'File')
+            }
+
+        }
+    }
+
     if ($Ensure -eq 'Present')
     {
         Write-Verbose ($script:localizedData.CheckingZoneMessage -f $Name, $Ensure)
 
-        $dnsServerZone = Get-DnsServerZone -Name $Name -ErrorAction SilentlyContinue
-
-        if ($dnsServerZone)
+        if ($dnsServerZone.type -eq 'Stub' -and $dnsServerZone.IsDSIntegrated -eq $false)
         {
+            # Compare the Desired master servers to the Existing master servers - if Existing doesn't match Desired, update the master servers for the zone.
+            $Comparison = Compare-Object -ReferenceObject $MasterServers -DifferenceObject $targetResource.MasterServers
 
-        $DesiredMasterServers = $MasterServers | Sort-Object
-        $CurrentMasterServers = $dnsServerZone.MasterServers | ForEach-Object { $_.IPAddressToString } | Sort-Object
-
-
-            if($CurrentMasterServers -join ',' -ne $DesiredMasterServers -join ',' )
+            if (-not $Comparison)
             {
                 # Update the Master Servers list
                 Set-DnsServerStubZone -Name $Name -MasterServers $MasterServers
@@ -180,17 +231,10 @@ function Set-TargetResource
         elseif (-not $dnsServerZone)
         {
             # Create the DNS Server zone.
-            Write-Verbose ($script:localizedData.AddingZoneMessage -f $Name)
-
             Add-DnsServerStubZone -Name $Name -ZoneFile $ZoneFile -MasterServers $MasterServers
-        }
-    }
-    elseif ($Ensure -eq 'Absent')
-    {
-        # Remove the DNS Server zone.
-        Write-Verbose ($script:localizedData.RemovingZoneMessage -f $Name)
 
-        Get-DnsServerZone -Name $Name | Remove-DnsServerZone -Force
+            Write-Verbose ($script:localizedData.AddingZoneMessage -f $Name)
+        }
     }
 
 } #end function Set-TargetResource
