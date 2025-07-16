3079ad92de082626dbc7a81e7f6fb7bf41a14aa4
ahpooch <ahpooch@hotmail.com>
2025-07-16 15:22:40 +0300

    Enhanced FilterScript for Get-DnsServerResourceRecord

--- source/Classes/020.DnsRecordCname.ps1
+++ source/Classes/020.DnsRecordCname.ps1
@@ -59,12 +59,8 @@ class DnsRecordCname : DnsRecordBase
         }
 
         $record = Get-DnsServerResourceRecord @dnsParameters -ErrorAction SilentlyContinue | Where-Object -FilterScript {
-            $FormattedHostNameAlias = $this.HostNameAlias
-            if ( -not $this.HostNameAlias.EndsWith('.'))
-            {
-                $FormattedHostNameAlias = $this.HostnameAlias + '.'
-            }
-            $_.RecordData.HostNameAlias -eq $FormattedHostNameAlias
+            # Ensure that HostNameAlias we using for filtering contains precisely one dot at the end.
+            $_.RecordData.HostNameAlias -eq $($this.HostNameAlias.Trim('.') + '.')
         }
 
         return $record
